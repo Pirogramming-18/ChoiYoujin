f30ad3468645cc1149cfb546be8090dc8d8f0987
Youjin Choi <kccyj6@gmail.com>
2023-01-07 02:21:58 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -8,7 +8,7 @@ class NotInRangeError(Exception):
   def __init__(self):
     super().__init__('1,2,3 중 하나를 입력하세요')
 
-while True:
+while num < 31:
   try:
     num_input=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
     if num_input not in range(1,4):
@@ -25,3 +25,5 @@ while True:
     for i in range(0,num_input):
       num = num + 1
       print(player + " : " + str(num))
+      if num == 31:
+        break
