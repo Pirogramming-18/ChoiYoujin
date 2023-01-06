515da803ef4613f5f190ef42691d39282c69ca64
Youjin Choi <kccyj6@gmail.com>
2023-01-07 03:12:50 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,7 +1,9 @@
+import random
+
 num = 0
 
-player1 = "PlayerA"
-player2 = "PlayerB"
+player1 = "Player"
+player2 = "Computer"
 player = player2
 
 class NotInRangeError(Exception):
@@ -18,25 +20,29 @@ def SwitchTurn():
 def brGame():
   global player
   global num
-  try:
-    num_input=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-    if num_input not in range(1,4):
-      raise NotInRangeError
-    SwitchTurn()
-  except ValueError:
-    print("정수를 입력하세요")
-  except NotInRangeError:
-    print("1,2,3 중 하나를 입력하세요")
-  else:
-    for i in range(0,num_input):
-      num = num + 1
-      print(player + " : " + str(num))
-      if num == 31:
-        break
+  while True:
+    try:
+      if player == "Player":
+        num_input=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+      elif player == "Computer":
+        num_input = random.randint(1,3)
+      if num_input not in range(1,4):
+        raise NotInRangeError
+    except ValueError:
+      print("정수를 입력하세요")
+    except NotInRangeError:
+      print("1,2,3 중 하나를 입력하세요")
+    else:
+      break
+
+  for i in range(0,num_input):
+    num = num + 1
+    print(player + " : " + str(num))
+    if num == 31:
+      break
+  SwitchTurn()
 
 while num < 31:
   brGame()
-
-SwitchTurn()
 
 print(player,"win!")
