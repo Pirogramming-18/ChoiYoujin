787371f82e0c252bd155facca42699c8378d8179
Youjin Choi <kccyj6@gmail.com>
2023-01-07 02:11:11 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,5 +1,9 @@
 num = 0
 
+player1 = "PlayerA"
+player2 = "PlayerB"
+player = player2
+
 class NotInRangeError(Exception):
   def __init__(self):
     super().__init__('1,2,3 중 하나를 입력하세요')
@@ -9,6 +13,10 @@ while True:
     num_input=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
     if num_input not in range(1,4):
       raise NotInRangeError
+    if player == player2:
+      player = player1
+    else:
+      player = player2
   except ValueError:
     print("정수를 입력하세요")
   except NotInRangeError:
@@ -16,5 +24,4 @@ while True:
   else:
     for i in range(0,num_input):
       num = num + 1
-      print("PlayerA : " + str(num))
-    break
+      print(player + " : " + str(num))
